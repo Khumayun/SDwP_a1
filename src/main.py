ba399a55b0799b121f1bfdf1492960fb7b0af583
Khumayun <huba4buba@gmail.com>
2021-09-12 23:01:56 +0300

    added class-based decorator

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import random
 from task1 import decorator_1
 from task2 import decorator_2
 from task3 import decorator_3
-from task4 import decorator_4
+from task4 import decorator_4, decorator_4_classed
 
 test1 = lambda a, b, c: '\nx1: '+str((-b+(b**2-4*a*c)**(1/2))/(2*a))+'\nx1: ' + str((-b+(b**2-4*a*c)**(1/2))/(2*a))
 
@@ -35,6 +35,7 @@ def funx(n=2, m=5):
         if i > max_val:
             max_val = i
 
+
 @decorator_2
 def funh(bar1, bar2=""):
     """
@@ -47,9 +48,11 @@ def funh(bar1, bar2=""):
 
 rank = []
 
+
 @decorator_3
 def fund(b):
     print('in a fund function', b)
+
 
 @decorator_3
 def funr(b):
@@ -57,14 +60,21 @@ def funr(b):
     for i in range(1000):
         pass
 
+
 @decorator_3
 def funn(b):
     print('in a funn function', b)
     for i in range(100000):
         pass
 
+
 @decorator_4
 def test(b):
+    print(1/b)
+
+
+@decorator_4_classed
+def test2(b):
     print(1/b)
 
 
@@ -85,3 +95,4 @@ if __name__ == '__main__':
     # for i, item in enumerate(rank, start=1):                        # task3
     #     print(item['name'], '\t\t', i, '\t', item['elapsed']+'s')   # task3
     # test(0)                                                     # task4
+    # test2(0)                                                    # task4
--- src/task4.py
+++ src/task4.py
@@ -6,8 +6,23 @@ def decorator_4(f):
 		try:
 			res = f(*args, **kwargs)
 		except Exception as e:
-			res = 'Exception name: \t| ' + str(e) + '\nFunction name: ' + f.__name__ + ' | ' + str(strftime('%d.%m.%Y %H:%M:%S'))
-			with open('log.txt', 'w') as file:
+			res = '\nException name: \t| ' + str(e) + '\nFunction name: ' + f.__name__ + ' | ' + str(strftime('%d.%m.%Y %H:%M:%S'))
+			with open('log.txt', 'a') as file:
 				file.write(res)
 		return res
 	return wrapper
+
+
+class decorator_4_classed:
+
+	def __init__(self, f):
+		self.f = f
+
+	def __call__(self, *args, **kwargs):
+		try:
+			res = self.f(*args, **kwargs)
+		except Exception as e:
+			res = '\nException name: \t| ' + str(e) + '\nFunction name: ' + self.f.__name__ + ' | ' + str(strftime('%d.%m.%Y %H:%M:%S'))
+			with open('log.txt', 'a') as file:
+				file.write(res)
+		return res
